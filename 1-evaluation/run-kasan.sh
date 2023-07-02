e6899f9ae263cbbcb5e068887c343ab1caf8fa5b
ppw <wzc@smail.nju.edu.cn>
2023-07-02 16:50:20 -0600

    fix `$KERNEL` path

--- 1-evaluation/run-kasan.sh
+++ 1-evaluation/run-kasan.sh
@@ -1,9 +1,9 @@
-KERNEL=../bzImage-kasan
+KERNEL=./images/bzImage-kasan
 IMAGE=./bullseye.img
 qemu-system-x86_64 \
     -m 2G \
     -smp 2 \
-    -kernel $KERNEL/arch/x86/boot/bzImage \
+    -kernel $KERNEL \
     -append "nokaslr console=ttyS0 root=/dev/sda earlyprintk=serial net.ifnames=0" \
     -drive file=$IMAGE,format=raw \
     -net user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10021-:22 \
